b29591951d234606a143668f91e0fa1201392459
Breno Moura <brenobatista1990@gmail.com>
2025-05-20 11:23:39 -0300

    .

--- requests/POST_To-Do_List.sh
+++ requests/POST_To-Do_List.sh
@@ -1,7 +1,7 @@
 curl --request POST \
     --url 'https://crud-autenticado-coral.vercel.app/todos' \
     --header 'Content-Type: application/json' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTlmZjZkZjlmODk4YTNkMzBkZDgyNSIsImlhdCI6MTc0Nzc1MDAzMywiZXhwIjoxNzQ3NzUzNjMzfQ.EyK2vXDVfF_5-747H0fCt89cAT7fdzRNrPzVll2lDR0' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTlmZjZkZjlmODk4YTNkMzBkZDgyNSIsImlhdCI6MTc0Nzc1MDk4OSwiZXhwIjoxNzQ3NzU0NTg5fQ.5YjyDAjK0QZtsdJ05buf7juQOpVe4jTv3cLzRsIhHN4' \
     --data '{
         "title": "Estudar Node.js",
         "description": "Assistir aulas e praticar exemplos."
