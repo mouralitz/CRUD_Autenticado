5245e515db2b3ef337f5b9d084b8cb28cdca32d8
Breno Moura <brenobatista1990@gmail.com>
2025-05-20 13:49:05 -0300

    E não nos deixeis cair em tentação, mas livrai-nos do mal.

--- requests/POST_To-Do_List.sh
+++ requests/POST_To-Do_List.sh
@@ -1,8 +1,7 @@
 curl --request POST \
     --url 'https://crud-autenticado-coral.vercel.app/api/tasks' \
     --header 'Content-Type: application/json' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTlmZjZkZjlmODk4YTNkMzBkZDgyNSIsImlhdCI6MTc0Nzc1Nzg1OCwiZXhwIjoxNzQ3NzYxNDU4fQ.DFpBaV5NsgKNYD4bIGvM7M3Eshg4xoNv5LRWSCjIeVA' \
-    --data '{
+    --header 'Authorization: Bearer "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTlmZjZkZjlmODk4YTNkMzBkZDgyNSIsImlhdCI6MTc0Nzc1OTYyMywiZXhwIjoxNzQ3NzYzMjIzfQ.fUpk-GIWF7dgbp3r2VbJTVF5jdeLfdodoIRm-Efqnq4
         "title": "Estudar Node.js",
         "description": "Assistir aulas e praticar exemplos."
     }'
